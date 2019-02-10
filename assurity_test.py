0d7e7e1c8c3e1e160ade3101aeb93a093e585ecd
Angel Rodriguez <angel.rdguez@gmail.com>
2019-02-10 21:12:08 +0100

    Adding python test skeleton

---
+++ assurity_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestAssurity(unittest.TestCase):
+
+    def test_name(self):
+        #TODO
+        print('dummy test')
+
+    def test_relist(self):
+        #TODO
+        print('dummy test')
+
+    def test_promotions_2x(self):
+        #TODO
+        print('dummy test')
+
+if __name__ == '__main__':
+
+    print('Hello Assurity!')
+    unittest.main()
